ef76b3b6d787768e8e264e86365635472a250ca0
Alumno <Alumno@CC23>
2017-03-02 17:41:27 +0100

    Finalizado el listado de programas el DOM

--- src/practicas/ProgramaDOM.java
+++ src/practicas/ProgramaDOM.java
@@ -1,5 +1,8 @@
 package practicas;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
@@ -10,6 +13,7 @@ import org.w3c.dom.NodeList;
 
 public class ProgramaDOM {
 	public static void main(String[] args) throws Exception {
+
 		DocumentBuilderFactory factoria = DocumentBuilderFactory.newInstance();
 
 		DocumentBuilder analizador = factoria.newDocumentBuilder();
@@ -17,18 +21,42 @@ public class ProgramaDOM {
 		Document d = analizador.parse(
 				"http://www.rtve.es/m/alacarta/programsbychannel/?media=tve&channel=la1&modl=canales&filterFindPrograms=todas");
 
-		NodeList lista, elementos = d.getElementsByTagName("li");
+		Pattern p = Pattern.compile("/m/alacarta/videos/(.+)/\\?media=tve");
+		Matcher m;
 
+		NodeList elementos = d.getElementsByTagName("li");
+
+		// System.out.println(elementos.getLength());
 		for (int i = 0; i < elementos.getLength(); i++) {
 
-			Node n = elementos.item(i);
+			NodeList nl = elementos.item(i).getChildNodes();
 
-			if (n.getLocalName().equals("p") && n.hasChildNodes()) {
-				// Este tiene contenido img, donde conseguimos el nombre y la
-				// url de la imagen
-			} else if (n.getLocalName().equals("a")) {
-				// Este es el a, sacar identificador
+			// System.out.println(nl.getLength());
+			for (int j = 0; j < nl.getLength(); j++) {
+				Node n = nl.item(j);
+				Element e;
+
+				// System.out.println(n.getNodeName());
+
+				if (n.getNodeName().equals("h3")) {
+					System.out.println(n.getTextContent());
+				} else if (n.getNodeName().equals("a")) {
+					e = (Element) n;
+					System.out.println(e.getAttribute("href"));
+
+					m = p.matcher(e.getAttribute("href"));
+					if (m.find()) {
+						System.out.println(m.group(1));
+
+						// Funcion que recoja todas las emisiones del programa
+					}
+				} else if (n.getNodeName().equals("p") && n.getFirstChild().getNodeName().equals("img")) {
+					e = (Element) n.getFirstChild();
+					System.out.println(e.getAttribute("src"));
+				}
 			}
+			System.out.println();
 		}
+
 	}
 }
